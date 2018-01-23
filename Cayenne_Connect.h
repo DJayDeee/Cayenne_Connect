3139196cb5d64f489b8c9e1852a7ac22f3b24ebc
DJayDeee <jean.daniel.lavoie.01@gmail.com>
2018-01-23 16:23:19 -0500

    Layout take 4 and for all

--- Cayenne_Connect.h
+++ Cayenne_Connect.h
@@ -17,75 +17,74 @@ SOFTWARE.
 #ifndef		_CAYENNE_CONNECT_H_
 #define		_CAYENNE_CONNECT_H_
 
-#include	<FS.h>														//https://github.com/pellepl/spiffs
-#include	<ArduinoJson.h>												//https://github.com/bblanchon/ArduinoJson
-#include	<ESP8266WiFi.h>												//https://github.com/esp8266/Arduino
-#include	<WiFiManager.h>												//https://github.com/kentaylor/WiFiManager
+#include	<FS.h>								//https://github.com/pellepl/spiffs
+#include	<ArduinoJson.h>							//https://github.com/bblanchon/ArduinoJson
+#include	<ESP8266WiFi.h>							//https://github.com/esp8266/Arduino
+#include	<WiFiManager.h>							//https://github.com/kentaylor/WiFiManager
 
-#define	TIMEOUT			180												// TIMEOUT to close the WiFi configuration portal if a SSID exist.
-#define	CONFIG_FILE		"/CC_config.json"								// CONFIG_FILE is the name of the Configuration file.
-void	saveConfigCallback(void);										// Callback helper to set shouldSaveConfig flag.
+#define	TIMEOUT		180							// TIMEOUT to close the WiFi configuration portal if a SSID exist.
+#define	CONFIG_FILE	"/CC_config.json"					// CONFIG_FILE is the name of the Configuration file.
+void	saveConfigCallback(void);						// Callback helper to set shouldSaveConfig flag.
 
 
 // Option for Connect(option)
-#define	STATIC_HOSTNAME		0											// Parameter to Connect() to static IP with hostname.
-#define	STATIC_NOHOSTNAME	1											// Parameter to Connect() to static IP without hostname.
-#define	DHCP_HOSTNAME		2											// Parameter to Connect() to dynamic IP with hostname.
-#define	DHCP_NOHOSTNAME		3											// Parameter to Connect() to dynamic IP without hostname.
+#define	STATIC_HOSTNAME		0						// Parameter to Connect() to static IP with hostname.
+#define	STATIC_NOHOSTNAME	1						// Parameter to Connect() to static IP without hostname.
+#define	DHCP_HOSTNAME		2						// Parameter to Connect() to dynamic IP with hostname.
+#define	DHCP_NOHOSTNAME		3						// Parameter to Connect() to dynamic IP without hostname.
 
-static	const	IPAddress	DHCPAddress	= IPAddress(0,0,0,0);			// IP adress used to force DHCP in STA mode.  https://github.com/esp8266/Arduino/pull/4145
-typedef struct _staticAddress {											// Container for the IP, GATEWAY, SUBNET address and HOSTNAME.
-	IPAddress	ip		= IPAddress(192,168,0,3);
+static	const	IPAddress	DHCPAddress = IPAddress(0,0,0,0);		// IP adress used to force DHCP in STA mode.  https://github.com/esp8266/Arduino/pull/4145
+typedef struct _staticAddress {							// Container for the IP, GATEWAY, SUBNET address and HOSTNAME.
+	IPAddress	ip	= IPAddress(192,168,0,3);
 	IPAddress	gateway	= IPAddress(192,168,0,1);
 	IPAddress	subnet	= IPAddress(255,255,255,0);
 	char		hostname[32];
 };
 
-typedef struct _MQTT_credential {										// Cayenne authentication info container.
+typedef struct _MQTT_credential {						// Cayenne authentication info container.
 	char username[48];
 	char password[48];
 	char clientID[48];
 };
 
-#define	PIN_LED			2												// Default led pin for debugging.
+#define	PIN_LED		2							// Default led pin for debugging.
 class LED {
   public:
-	LED(const int _pin = PIN_LED) {	led_pin = _pin;	pinMode(led_pin, OUTPUT);	digitalWrite(led_pin, HIGH);	}
-	void	off(void)		{	digitalWrite(led_pin, HIGH);							}
-	void	on(void)		{	digitalWrite(led_pin, LOW);								}
-	void	toggle(void)	{	digitalWrite(led_pin, !digitalRead(led_pin));			}	
+	LED(const int _pin = PIN_LED)	{ led_pin = _pin;	pinMode(led_pin, OUTPUT);	digitalWrite(led_pin, HIGH);	}
+	void	off(void)		{ digitalWrite(led_pin, HIGH);			}
+	void	on(void)		{ digitalWrite(led_pin, LOW);			}
+	void	toggle(void)		{ digitalWrite(led_pin, !digitalRead(led_pin));	}
   private:
-	int		led_pin;
+	int	led_pin;
 };
 
 class Cayenne_Connect {
   public:
-	Cayenne_Connect(void);												// Restore configuration, connect WiFi whit static IP, start WiFiManager, save configuration if needed and reconnect WiFi whit dynamic IP and hostname.
-	void		setDebugOutput(const bool _debug) {	debug = _debug;	}	// Called to enable/disable the debug information over serial.
-	
+	Cayenne_Connect(void);							// Restore configuration, connect WiFi whit static IP, start WiFiManager, save configuration if needed and reconnect WiFi whit dynamic IP and hostname.
+	void		setDebugOutput(const bool _debug) { debug = _debug;	} // Called to enable/disable the debug information over serial.
+
 	char*		getMQTTusername(void) {	return MQTT_credential.username;	}
 	char*		getMQTTpassword(void) {	return MQTT_credential.password;	}
 	char*		getMQTTclientID(void) {	return MQTT_credential.clientID;	}
-	
-	static	bool		shouldSaveConfig;								// Flag for saving data.
-	
+
+	static	bool	shouldSaveConfig;					// Flag for saving data.
+
   private:
-	char				ssid[32];
-	char				pass[32];
+	char			ssid[32];
+	char			pass[32];
 	_staticAddress		staticAddress;
 	_MQTT_credential	MQTT_credential;
-	bool				debug = true;
-	
-	bool		readWiFiConfigFile(void);								// Restore configuration from the FileSystem.
-	bool		writeWiFiConfigFile(void);								// Save WiFi configuration to the FileSystem.
-	
-	bool		Connect(const int option = STATIC_HOSTNAME) const;		// Force WiFi connection, see #define for option.
-	void		OpenPortal(void);										// Configure the WiFi via WiFiManager portal.
+	bool			debug = true;
 
-	template <typename Generic>											// Debug channel enable if debug is set.
+	bool		readWiFiConfigFile(void);				// Restore configuration from the FileSystem.
+	bool		writeWiFiConfigFile(void);				// Save WiFi configuration to the FileSystem.
+
+	bool		Connect(const int option = STATIC_HOSTNAME) const;	// Force WiFi connection, see #define for option.
+	void		OpenPortal(void);					// Configure the WiFi via WiFiManager portal.
+
+	template <typename Generic>						// Debug channel enable if debug is set.
 	void		DEBUG_CC(Generic text) const;
 
 };
 
 #endif // _CAYENNE_CONNECT_H_
-
--- example/Cayenne_test/Cayenne_test.ino
+++ example/Cayenne_test/Cayenne_test.ino
@@ -3,24 +3,24 @@
 * Date	:		2017-Dec-14
 * By	:		Jean-Daniel Lavoie
 * Description :	Inspired from kentaylor/WiFiManager (forked from tzapu/WiFiManager)
-*				Manage WiFi connection at every reboot for 60 seconds whit fixed IP.
-*				Connect to cayenne.mydevices.com
+*		Manage WiFi connection at every reboot for 60 seconds whit fixed IP.
+*		Connect to cayenne.mydevices.com
 ****************************************************************************************/
 
-#include <Cayenne_Connect.h>
+#include <Cayenne_Connect.h>							//https://github.com/DJayDeee/Cayenne_Connect
 
 //#define CAYENNE_DEBUG
 #define CAYENNE_PRINT Serial
-#include <CayenneMQTTESP8266.h>
+#include <CayenneMQTTESP8266.h>							//https://github.com/myDevicesIoT/Cayenne-MQTT-ESP
 
-char username[48];																	// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[48];								// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
 char password[48];
 char clientID[48];
 
-#include <queue>																	// std::queue
+#include <queue>								// std::queue
 typedef enum user_request {
-	RESTART,																		// Reset event.
-	SEND,																			// Send event.
+	RESTART,								// Reset event.
+	SEND,									// Send event.
 } user_request;
 std::queue<user_request> request_queue;
 
@@ -30,20 +30,20 @@ LED Debug_LED;
 // setup()		Put your setup code here, to run once:
 //***************************************************************************************
 void setup(void) {
-	Debug_LED.on();																	// Turn on LED during BOOT.
+	Debug_LED.on();								// Turn on LED during BOOT.
 
-	Serial.begin(74880);															// Start serial for debug.
-	Serial.println(F(" "));															// First print is garbage.
+	Serial.begin(74880);							// Start serial for debug.
+	Serial.println(F(" "));							// First print is garbage.
 
-	Cayenne_Connect my_connection;													// Manage WiFi connection whit staticIP.
-	if (WiFi.isConnected()) {														// Turn LED OFF if connected.
+	Cayenne_Connect my_connection;						// Manage WiFi connection whit staticIP.
+	if (WiFi.isConnected()) {						// Turn LED OFF if connected.
 		Debug_LED.off();
 	}
 
 	strcpy(username, my_connection.getMQTTusername());
 	strcpy(password, my_connection.getMQTTpassword());
 	strcpy(clientID, my_connection.getMQTTclientID());
-	Cayenne.begin(username, password, clientID);									// Start Cayenne MQTT client.
+	Cayenne.begin(username, password, clientID);				// Start Cayenne MQTT client.
 }
 
 
@@ -56,15 +56,15 @@ void loop() {
 
 	if (!request_queue.empty()) {
 		switch (request_queue.front()) {
-			case SEND:																// Update Cayenne dashboard.
+			case SEND:						// Update Cayenne dashboard.
 				Debug_LED.toggle();
-// Put your Cayenne.virtualWrite(...); here	
-				Cayenne.virtualWrite(1, false);										// Update cayenne_send button.
+// Put your Cayenne.virtualWrite(...); here
+				Cayenne.virtualWrite(1, false);			// Update cayenne_send button.
 				Serial.println(F("Cayenne server updated."));
 				break;
 
-			case RESTART:															// Reset!!
-				Cayenne.virtualWrite(0, false);										// Update cayenne_restart button.
+			case RESTART:						// Reset!!
+				Cayenne.virtualWrite(0, false);			// Update cayenne_restart button.
 				Serial.println(F("!!Restarting NOW!!"));
 				ESP.restart();
 				delay(5000);
@@ -97,5 +97,3 @@ CAYENNE_IN_DEFAULT() {
 	CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
 	//Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
 }
-
-
